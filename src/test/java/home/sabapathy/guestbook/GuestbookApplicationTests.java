c3c1698a2c15da1d42041568f5cb6fd3e300d08a
Selva Sabapathy <selva.sabapathy@gmail.com>
2021-04-05 19:07:35 -0400

    Initial commit

---
+++ src/test/java/home/sabapathy/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package home.sabapathy.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
