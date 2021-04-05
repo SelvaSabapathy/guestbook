ffe7658501ab58c8c5a0404614189dcc0402db17
Selva Sabapathy <selva.sabapathy@gmail.com>
2021-04-05 19:28:10 -0400

    Skeleton controller tests

---
+++ README.md
@@ -0,0 +1,13 @@
+Guest Book
+
+Features
+Any visitor can post their name and a comment to the Guestbook.
+All visitors can see a list of every entry in the Guestbook.
+
+REST Endpoint: /guestbook
+    GET gets all the entries from the guestbook
+    POST will add a new entry in the guestbook
+
+End Point  | Method | Data
+/guestbook | GET    | []
+/guestbook | POST   | { "name": "" | "comment": "" }
---
+++ src/test/java/home/sabapathy/guestbook/controller/GuestBookControllerTest.java
@@ -0,0 +1,18 @@
+package home.sabapathy.guestbook.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GuestBookControllerTest {
+
+    @Test
+    public void readEntries() {
+    }
+
+    @Test
+    public void addEntry() {
+    }
+}
